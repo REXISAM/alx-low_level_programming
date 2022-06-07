876bf107b37031257ef0b6e87a956c4708624fc3
Onele Samuel <resilientsamuel@gmail.com>
2022-06-07 08:07:51 -0700

    git commit -m

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - entry point
+ * @c: takes in a character
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
