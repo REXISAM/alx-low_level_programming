2de3dcdcbfaaf5af626a1d8eaa130a5d73a3d215
Onele Samuel <resilientsamuel@gmail.com>
2022-06-12 23:26:53 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2
+ * @str: type
+ *
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
+	}
+	_putchar('\n');
+}
